a7466dcb02af612776af17c7cbfbff67face55db
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-08 19:31:33 +0700

    2nd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d72fd7d441952a2"; }
+/******/ 		__webpack_require__.h = function() { return "c7f13617b61d5e57"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
